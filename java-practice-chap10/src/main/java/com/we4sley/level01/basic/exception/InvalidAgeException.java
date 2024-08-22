cc862d908977d8278f6aa7825e4b9960af72f630
Younggi Kim <galaxy138742@gmail.com>
2024-08-22 22:35:55 +0900

    feat: chap 10

---
+++ java-practice-chap10/src/main/java/com/we4sley/level01/basic/exception/InvalidAgeException.java
@@ -0,0 +1,7 @@
+package com.we4sley.level01.basic.exception;
+
+public class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
